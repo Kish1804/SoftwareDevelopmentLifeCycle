fed80e821bad7f1d6c7189573bd0ff6b8dcf3aea
unknown <you@example.com>
2022-10-19 09:48:13 +0530

    updated third phase

--- src/coding/ThirdPhase.java
+++ src/coding/ThirdPhase.java
@@ -5,6 +5,7 @@ public class ThirdPhase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("thirdd phase of software development life cycle is coding");
+		System.out.println("code: high level design / Low level design");
 	}
 
 }
