ae73d53a22cfa124ebda08b89fb6e17ed4250bb3
unknown <you@example.com>
2022-10-19 09:28:16 +0530

    third phase created

---
+++ src/coding/ThirdPhase.java
@@ -0,0 +1,10 @@
+package coding;
+
+public class ThirdPhase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("thirdd phase of software development life cycle is coding");
+	}
+
+}
