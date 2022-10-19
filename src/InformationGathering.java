86141e208840d2236efdda783c95e25bba9c4c80
unknown <you@example.com>
2022-10-19 08:26:34 +0530

    info gathering

---
+++ src/InformationGathering.java
@@ -0,0 +1,10 @@
+
+public class InformationGathering {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("it is first phase of sdlc");
+
+	}
+
+}
