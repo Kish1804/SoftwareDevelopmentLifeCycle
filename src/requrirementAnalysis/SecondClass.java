50003fd13564f0059a00fe2f436364b64e0e1cbd
unknown <you@example.com>
2022-10-19 08:51:40 +0530

    second phase created

---
+++ src/requrirementAnalysis/SecondClass.java
@@ -0,0 +1,11 @@
+package requrirementAnalysis;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second phase of software development life cycle is Requirement analysis");
+		System.out.println("business Analyst will prepare BRS based on the requirements he gathers from the client.");
+	}
+
+}
